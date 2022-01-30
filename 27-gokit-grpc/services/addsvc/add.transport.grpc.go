9a14771d416f2b273e82c543a4dc9674f43d4508
kevinnguyenai <kevin.nguyen.ai@gmail.com>
2022-01-30 20:33:14 +0700

    protobuf

--- 27-gokit-grpc/services/addsvc/add.transport.grpc.go
+++ 27-gokit-grpc/services/addsvc/add.transport.grpc.go
@@ -21,7 +21,7 @@ import (
 	"log"
 
 	grpctransport "github.com/go-kit/kit/transport/grpc"
-	pbaddsvc "github.com/kevinnguyenai/golang-tutorials/27-gokit-grpc/pb/addsvc"
+	"github.com/kevinnguyenai/golang-tutorials/27-gokit-grpc/pb/addsvc"
 	stdopentracing "github.com/opentracing/opentracing-go"
 	stdzipkin "github.com/openzipkin/zipkin-go"
 	"google.golang.org/grpc"
@@ -45,12 +45,12 @@ func NewGRPCClient(conn *grpc.ClientConn, otTracer stdopentracing.Tracer, zipkin
 
 func encodeGRPCSumRequest(_ context.Context, request interface{}) (interface{}, error) {
 	req := request.(SumRequest)
-	return &pbaddsvc.SumRequest{A: int64(req.A), B: int64(req.B)}, nil
+	return &addsvc.SumRequest{A: int64(req.A), B: int64(req.B)}, nil
 }
 
 func encodeGRPCConcatRequest(_ context.Context, request interface{}) (interface{}, error) {
 	req := request.(ConcatRequest)
-	return &pbaddsvc.ConcatRequest{A: string(req.A), B: string(req.B)}, nil
+	return &addsvc.ConcatRequest{A: string(req.A), B: string(req.B)}, nil
 }
 
 // These annoying helper functions are required to translate Go error types to
